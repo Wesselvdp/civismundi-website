4e06700cfac2cfd9d70424723b66ab7e22b12376
Roy Broertjes <you@example.com>
2020-09-29 13:09:39 +0200

    build via studio

---
+++ studio/src/dashboardConfig/index.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+      {
+      name: 'netlify',
+      options: {
+        title: 'My Netlify deploys',
+        sites: [
+          {
+            title: 'Sanity CMS',
+            apiId: '94e5f884-bc07-46f1-bf95-ecf7290bae6e',
+            buildHookId: '5f7314fb50243b624ba41d7e',
+            name: 'sanity-gatsby-civismundi-studio-cms',
+          }
+        ]
+      }
+    }
+  ]
+}
