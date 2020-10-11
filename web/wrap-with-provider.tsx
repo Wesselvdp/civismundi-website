a3313a0a16dd9f717c0918122af6cb9fbb8c647d
Roy <roy@iMac-van-Roy.local>
2020-10-11 17:37:22 +0200

    mobile fix?

--- web/wrap-with-provider.tsx
+++ web/wrap-with-provider.tsx
@@ -5,11 +5,11 @@ import { Provider } from 'react-redux'
 
 import * as reducers from './src/reducers'
 
-const composeEnhancers = typeof window !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
+const composeEnhancers =  (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 
 const combinedReducers = combineReducers({ ...reducers })
 export default ({ element }) => {
-    const store = createStore(combinedReducers, undefined, composeEnhancers(applyMiddleware(thunk)))
-
+    const store = createStore(combinedReducers, composeEnhancers(applyMiddleware(thunk)))
+ 
     return <Provider store={store}>{element}</Provider>
 }
