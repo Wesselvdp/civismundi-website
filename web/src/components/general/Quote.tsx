d6f36c52b046edf89f20e47518850c079f9ea20f
Roy <roy@iMac-van-Roy.local>
2020-12-08 12:59:51 +0100

    fix quote

--- web/src/components/general/Quote.tsx
+++ web/src/components/general/Quote.tsx
@@ -21,21 +21,22 @@ const Quote = ({ project, className = '', animated = true, ...rest }) => {
           })
         )
 
-      if (content) {
-        content = content.toUpperCase()
-        setContent(content)
-        
-        setQuotee(
-          get(project, 'node.quote.content')
-            ? get(project, 'node.quote.quotee', '').toUpperCase()
-            : ''
-        )
-        setType(
-          project.node.quote && project.node.quote.content ? 'quote' : 'clients'
-        )
-      }
+      if (!content) content = ''
+      content = content.toUpperCase()
+      setContent(content)
+
+      setQuotee(
+        get(project, 'node.quote.content')
+          ? get(project, 'node.quote.quotee', '').toUpperCase()
+          : ''
+      )
+      setType(
+        project.node.quote && project.node.quote.content ? 'quote' : 'clients'
+      )
     }
   }, [project])
+
+  if (!content) return null
 
   return (
     <>
