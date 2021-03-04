2add23973ab3938b6c7fb10fc893d3638c42b055
Roy <roy@iMac-van-Roy.local>
2021-03-04 16:30:07 +0100

    text effect

--- web/src/components/world/index.tsx
+++ web/src/components/world/index.tsx
@@ -1,5 +1,6 @@
 import World from './World'
 import Container from './Container'
 import Galaxy from './Galaxy'
+import Title from './Title'
 
-export { World, Container, Galaxy }
+export { World, Container, Galaxy, Title }
