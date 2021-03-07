287ba5aeb9ac34eb3e05156d031c4b13d89b67e6
Roy <roy@iMac-van-Roy.local>
2021-03-07 18:46:40 +0100

    border fade south america + africa

--- web/src/world/objects/Regions.tsx
+++ web/src/world/objects/Regions.tsx
@@ -1,8 +1,9 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { clone } from 'lodash'
 
 import BaseObject from './BaseObject'
-import World from '..';
+import World from '..'
 
 const videoUrls = [
   'stargazing.mp4',
@@ -16,47 +17,55 @@ const videoUrls = [
   'columbus.mp4',
   'dont-start-now.mp4',
   'lebron.mp4',
-  'spa-night.mp4'
+  'spa-night.mp4',
 ]
+
+/* eslint-disable prettier/prettier */
+const alphaMapUrls = {
+  'Land004_Mesh001' : 'Africa.jpg',
+  'Land001_Mesh001': 'South-America.jpg'
+}
+/* eslint-enable prettier/prettier */
 
 export default class Regions extends BaseObject {
   videos: any[] = []
+  alphaMaps: any[] = []
 
   constructor(world: World) {
-    super(world);
+    super(world)
 
-    this.createVideoTextures();
-    this.loadFile();
+    this.createAlphaMaps()
+    this.createVideoTextures()
+    this.loadFile()
   }
 
   loadFile() {
-    const that = this
-
     const loader = new GLTFLoader(THREE.DefaultLoadingManager)
-    loader.load('/Globe.glb', ( gltf ) => {
+    loader.load('/Globe.glb', (gltf) => {
       gltf.scene.children.forEach((child: any) => {
         if (child instanceof THREE.Mesh) {
           child.name === 'Globe_Mesh001'
-            ? that.setWorldTexture(child)
-            : that.setVideoTexture(child)
+            ? this.setWorldTexture(child)
+            : this.setVideoTexture(child)
         }
       })
 
-      gltf.scene.scale.set(100.5, 100.5, 100.5)
-      gltf.scene.rotation.y = 1.110 * Math.PI // magic
+      console.log('children', gltf.scene.children)
 
-      that.object = gltf.scene
-      that.world.globe.scene().add( gltf.scene )
+      gltf.scene.scale.set(100.5, 100.5, 100.5)
+      gltf.scene.rotation.y = 1.11 * Math.PI // magic
+
+      this.object = gltf.scene
+      this.world.globe.scene().add(gltf.scene)
     })
   }
 
   createVideoTextures() {
     const that = this
 
-    // create video textures
-    videoUrls.forEach((slug: string, i: number)=> {
-      const video = document.createElement( 'video' );
-      video.setAttribute('playsinline', 'playsinline');
+    videoUrls.forEach((slug: string, i: number) => {
+      const video = document.createElement('video')
+      video.setAttribute('playsinline', 'playsinline')
       video.src = `/videos/${slug}`
       video.muted = true
       video.crossOrigin = 'anonymous'
@@ -64,8 +73,11 @@ export default class Regions extends BaseObject {
 
       video.load()
       video.addEventListener('ended', () => {
-        const continents = that.object.children
-          .filter((child: any) => child instanceof THREE.Mesh && that.videos[i].active.includes(child.uuid))
+        const continents = that.object.children.filter(
+          (child: any) =>
+            child instanceof THREE.Mesh &&
+            that.videos[i].active.includes(child.uuid)
+        )
 
         continents.forEach((continent: THREE.Mesh) => {
           that.setVideoTexture(continent)
@@ -74,20 +86,45 @@ export default class Regions extends BaseObject {
         that.videos[i].active = []
       })
 
-      const texture = new THREE.VideoTexture( video );
-      texture.minFilter = THREE.LinearFilter;
-      texture.magFilter = THREE.LinearFilter;
-      texture.format = THREE.RGBFormat;
-      texture.flipY = false;
+      const texture = new THREE.VideoTexture(video)
+      texture.minFilter = THREE.LinearFilter
+      texture.magFilter = THREE.LinearFilter
+      texture.format = THREE.RGBFormat
+      texture.flipY = false
 
-      const material = new THREE.MeshLambertMaterial({ side: THREE.FrontSide, map: texture, opacity: 0.65, transparent: true });
+      const material = new THREE.MeshBasicMaterial({
+        side: THREE.FrontSide,
+        map: texture,
+        opacity: 0.65,
+        transparent: true,
+      })
       that.videos.push({ material, video, active: [] })
+    })
+  }
+
+  createAlphaMaps() {
+    // create video textures
+    this.alphaMaps = Object.values(alphaMapUrls).map((url: string) => {
+      const img = document.createElement('img')
+      const alphaMap = new THREE.Texture(img)
+
+      img.onload = function () {
+        alphaMap.needsUpdate = true
+      }
+
+      img.src = `/alpha-map/${url}`
+      alphaMap.minFilter = THREE.LinearFilter
+      alphaMap.magFilter = THREE.LinearFilter
+      alphaMap.format = THREE.RGBFormat
+      alphaMap.flipY = false
+
+      return { map: alphaMap, url }
     })
   }
 
   setVideoTexture(child: THREE.Mesh) {
     // Find free video
-    const free = this.videos.filter(obj => obj.active.length === 0)
+    const free = this.videos.filter((obj) => obj.active.length === 0)
     const i = Math.floor(Math.random() * free.length)
 
     if (free.length) {
@@ -96,14 +133,26 @@ export default class Regions extends BaseObject {
       obj.video.play()
       obj.active.push(child.uuid)
 
-      child.material = obj.material
+      const mat = clone(obj.material)
+      const alphaMap = this.alphaMaps.find(
+        (obj: any) => obj.url === alphaMapUrls[child.name]
+      )
+      if (alphaMap) {
+        mat.alphaMap = alphaMap.map
+        mat.alphaMap.needsUpdate = true
+      }
+
+      child.material = mat
     }
   }
 
   setWorldTexture(globe: THREE.Mesh) {
-    const text = new THREE.TextureLoader().load( '/earth-blue-marble-alt.jpg');
-    text.flipY = false;
+    const text = new THREE.TextureLoader().load('/earth-blue-marble-alt.jpg')
+    text.flipY = false
 
-    globe.material = new THREE.MeshLambertMaterial({ side: THREE.DoubleSide, map: text });
+    globe.material = new THREE.MeshLambertMaterial({
+      side: THREE.DoubleSide,
+      map: text,
+    })
   }
 }
