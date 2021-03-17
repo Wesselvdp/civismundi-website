c62f5636422a3a3f195d876d6258c2eb42715986
Roy <roy@iMac-van-Roy.local>
2021-03-17 01:30:45 +0100

    loader

--- web/src/components/html/Loader/index.tsx
+++ web/src/components/html/Loader/index.tsx
@@ -2,12 +2,10 @@
 
 import React, { useState, useEffect, useRef } from 'react'
 import styled from 'styled-components'
-import { useSelector, useDispatch } from 'react-redux'
 import Lottie from 'react-lottie'
 import TWEEN from '@tweenjs/tween.js'
 
 import animationData from './lottie-globe.json'
-import { SET_READY } from 'src/actions/types'
 
 const CIRCLE_SIZE = 60
 const CIRCUMFERENCE = CIRCLE_SIZE * 2 * Math.PI
@@ -21,44 +19,13 @@ const defaultOptions = {
   },
 }
 
-const Loader = () => {
-  const world = useSelector((state: any) => state.world)
-  const dispatch = useDispatch()
-
-  const ring = useRef(null)
-  const finalRing = useRef(null)
-  const ringTween = useRef(null)
+const Loader = ({ progress, onFinish }) => {
+  const ring = useRef()
+  const finalRing = useRef()
+  const tween = useRef()
 
   const [pseudoProgress, setPseudoProgress] = useState(0)
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    if (!loading && !world.ready) {
-      setTimeout(() => {
-        dispatch({ type: SET_READY, ready: true })
-      }, 1000)
-    }
-  }, [loading])
-
-  useEffect(() => {
-    if (ringTween.current) ringTween.current.stop()
-
-    ringTween.current = new TWEEN.Tween({ progress: pseudoProgress })
-      .to({ progress: world.progress }, 500)
-      .onUpdate((d) => {
-        setPseudoProgress(d.progress)
-      })
-      .easing(TWEEN.Easing.Cubic.InOut)
-      .start(0)
-  }, [world.progress])
-
-  useEffect(() => {
-    ring.current.style.strokeDashoffset = CIRCUMFERENCE - pseudoProgress * CIRCUMFERENCE
-
-    if (pseudoProgress === 1) {
-      startFinalRing()
-    }
-  }, [pseudoProgress])
+  const [show, setShow] = useState(true)
 
   const startFinalRing = () => {
     new TWEEN.Tween({ progress: 0 })
@@ -72,56 +39,84 @@ const Loader = () => {
         finalRing.current.style.strokeDashoffset = offset
       })
       .onComplete(() => {
-          setLoading(false)
+        setShow(false)
+        onFinish(true)
       })
       .easing(TWEEN.Easing.Cubic.InOut)
       .start()
   }
 
+  function animate(time) {
+    requestAnimationFrame(animate)
+    TWEEN.update(time)
+  }
+
+  useEffect(() => {
+    requestAnimationFrame(animate)
+  }, [])
+
+  useEffect(() => {
+    if (tween.current) tween.current.stop()
+
+    tween.current = new TWEEN.Tween({ progress: pseudoProgress })
+      .to({ progress: progress }, 1000)
+      .onUpdate((d) => {
+        setPseudoProgress(d.progress)
+      })
+      .easing(TWEEN.Easing.Cubic.InOut)
+      .start()
+  }, [progress])
+
+  useEffect(() => {
+    ring.current.style.strokeDashoffset = CIRCUMFERENCE - pseudoProgress * CIRCUMFERENCE
+
+    if (pseudoProgress === 1) {
+      console.log('starting final ring!')
+      startFinalRing()
+    }
+  }, [pseudoProgress])
+
   return (
-    <Container className={!loading ? 'hidden' : ''}>
-        <div>
-          <div className="lottie-wrapper">
-            <Lottie
-              options={defaultOptions}
-              height={87}
-              width={87}
-              isStopped={false}
-              isPaused={false}
+    <Container className={!show ? 'hidden' : ''}>
+      <div>
+        <div className="lottie-wrapper">
+          <Lottie
+            options={defaultOptions}
+            height={87}
+            width={87}
+            isStopped={false}
+            isPaused={false}
+          />
+          <svg
+            className="progress-ring"
+            width={CIRCLE_SIZE * 2}
+            height={CIRCLE_SIZE * 2}
+          >
+            <circle
+              ref={ring}
+              className="progress-ring__circle"
+              stroke="rgba(255, 255, 255, 1)"
+              strokeWidth="1"
+              fill="transparent"
+              r={CIRCLE_SIZE - 2}
+              cx={CIRCLE_SIZE}
+              cy={CIRCLE_SIZE}
             />
-            <svg
-              className="progress-ring"
-              width={CIRCLE_SIZE * 2}
-              height={CIRCLE_SIZE * 2}
-            >
-              <circle
-                ref={ring}
-                className="progress-ring__circle"
-                stroke="rgba(255, 255, 255, 1)"
-                strokeWidth="1"
-                fill="transparent"
-                r={CIRCLE_SIZE - 2}
-                cx={CIRCLE_SIZE}
-                cy={CIRCLE_SIZE}
-              />
-              <circle
-                ref={finalRing}
-                className="progress-ring__circle"
-                stroke="rgba(255, 255, 255, 1)"
-                strokeWidth="2"
-                fill="transparent"
-                r={CIRCLE_SIZE - 2}
-                cx={CIRCLE_SIZE}
-                cy={CIRCLE_SIZE}
-              />
-            </svg>
-          </div>
-          <p className={world.ready && 'hidden'}>{`${parseInt(
-            pseudoProgress * 100,
-            10
-          )}%`}</p>
+            <circle
+              ref={finalRing}
+              className="progress-ring__circle"
+              stroke="rgba(255, 255, 255, 1)"
+              strokeWidth="2"
+              fill="transparent"
+              r={CIRCLE_SIZE - 2}
+              cx={CIRCLE_SIZE}
+              cy={CIRCLE_SIZE}
+            />
+          </svg>
         </div>
-      </Container>
+        <p>{`${parseInt(pseudoProgress * 100, 10)}%`}</p>
+      </div>
+    </Container>
   )
 }
 
--- web/src/layouts/index.tsx
+++ web/src/layouts/index.tsx
@@ -1,18 +1,25 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-import { Navigation, Galaxy, Content } from '../components/html'
+import { Navigation, Galaxy, Content, Loader } from '../components/html'
 import { Canvas } from '../components/webgl'
 
 const Layout = () => {
   const interactionTimer = useRef()
   const [showContent, setShowContent] = useState(false)
+  const [progress, setProgress] = useState(0)
+  const [ready, setReady] = useState(false)
 
   return (
     <>
-      <Navigation />
+      <Navigation ready={ready} />
       <Galaxy />
+      <Loader progress={progress} onFinish={setReady} />
       <Content show={showContent} setShow={setShowContent} />
-      <Canvas timerRef={interactionTimer} onTimerEnd={setShowContent} />
+      <Canvas
+        timerRef={interactionTimer}
+        onTimerEnd={setShowContent}
+        onProgress={setProgress}
+      />
     </>
   )
 }
