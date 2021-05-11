dd1f15fd30c98ab43aa6505a96b969c8e71f4069
Roy <roy@iMac-van-Roy.local>
2021-05-11 14:41:55 +0200

    alpha maps

--- web/src/components/webgl/Globe/data.tsx
+++ web/src/components/webgl/Globe/data.tsx
@@ -9,31 +9,37 @@ export const textures: any = {
   },
   // SA
   'Land001_Mesh001': {
+    alphaMap: 'alpha-sa.jpg',
     video: 'vid6.mp4',
     videoSanity: 'https://cdn.sanity.io/files/aail6zsu/dev/8219a0772b8fdb2df3cf418881c4b6a5b545d380.mp4'
   },
   // Africa
   'Land006_Mesh001': {
+    alphaMap: 'alpha-africa.jpg',
     video: 'vid7.mp4',
     videoSanity: 'https://cdn.sanity.io/files/aail6zsu/dev/5e8f124a1bf59792a60b59b07333bef3c01c77b4.mp4'
   },
   // Russia
   'Land002_Mesh001': {
+    alphaMap: 'alpha-russia.jpg',
     video: 'vid4.mp4',
     videoSanity: 'https://cdn.sanity.io/files/aail6zsu/dev/569f22b69aa7aba92434eead87e5e9202e2db638.mp4'
   },
   // Europe
   'Land_Mesh002': {
+    alphaMap: 'alpha-europe.jpg',
     video: 'vid2.mp4',
     videoSanity: 'https://cdn.sanity.io/files/aail6zsu/dev/84d796007f53af33720f0cec277dfcc9b917ff78.mp4'
   },
   // south pole
   'Land005_Mesh001' : {
+    alphaMap: 'alpha-southpole.jpg',
     video: 'vid1.mp4',
     videoSanity: 'https://cdn.sanity.io/files/aail6zsu/dev/aee96a9b4d23630963dafb0aa594c5aa6ffe1fd3.mp4',
   },
   // Australia
   'Land003_Mesh001' : {
+    alphaMap: 'alpha-australia.jpg',
     video: 'vid3.mp4',
     videoSanity: 'https://cdn.sanity.io/files/aail6zsu/dev/d87a2ff51ccd755e3c3e17b93d796a14d1c2eab2.mp4'
   }
